d25fcd77b1e3f5b4bfad5376f1a98fcb53638379
caio <caio.lima@youxgroup.com.br>
2023-04-27 13:28:37 -0300

    sei la

---
+++ .quasar/quasar-user-options.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.config.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+
+
+import {Dialog} from 'quasar'
+
+
+
+export default { config: {"brand":{"primary":"#e01b2b","secondary":"#26A69A","accent":"#9C27B0","dark":"#1d1d1d","dark-page":"#121212","positive":"#21BA45","negative":"#C10015","info":"#31CCEC","warning":"#F2C037"}},plugins: {Dialog} }
+
