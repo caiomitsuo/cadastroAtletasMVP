8020f3fb2215d6b25a51d658b6eb1263e984c608
Caio.lima <caio.lima@youxgroup.com.br>
2023-04-30 22:06:42 -0300

    adicionando menu lateral e configurando rotas do crud

--- quasar.config.js
+++ quasar.config.js
@@ -40,7 +40,6 @@ module.exports = configure(function (/* ctx */) {
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
       // 'ionicons-v4',
-      // 'mdi-v5',
       // 'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
@@ -48,7 +47,8 @@ module.exports = configure(function (/* ctx */) {
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'roboto-font', // optional, you are not bound to it
-      'material-icons' // optional, you are not bound to it
+      'material-icons', // optional, you are not bound to it
+      'mdi-v5'
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
